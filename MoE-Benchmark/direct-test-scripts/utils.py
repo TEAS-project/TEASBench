16c229d50ba8644caf37e9227889c292825b1af4
arno <a.proeme@epcc.ed.ac.uk>
2026-04-17 17:12:47 +0000

    Short name for longbench

--- MoE-Benchmark/direct-test-scripts/utils.py
+++ MoE-Benchmark/direct-test-scripts/utils.py
@@ -16,8 +16,13 @@ MODEL_SHORT_NAME_MAP={
     "Qwen3-235B-A22B-Instruct-2507": "qwen3-235b",
     "DeepSeek-R1": "deepseek-r1",
     "Kimi-K2.5": "kimi-k2.5"
-
 }
+
+DATASET_SHORT_NAME_MAP={
+    "gsm8k": "gsm8k",
+    "arena-hard":, "arena-hard",
+    "longbench_v1": "longbench"
+    }
 
 HF_MODEL_MAP={
     "gpt-oss-20b": "unsloth/gpt-oss-20b",
@@ -44,7 +49,7 @@ EIDF_GPU_MAP={
 
 
 def get_run_name(inference_engine, model, dataset, num_samples, gpu, num_gpu, batch_size):
-    name = f"{inference_engine}_{MODEL_SHORT_NAME_MAP[model]}_{dataset}_ns{num_samples}_{gpu}x{num_gpu}"
+    name = f"{inference_engine}_{MODEL_SHORT_NAME_MAP[model]}_{DATASET_SHORT_NAME[dataset]}_ns{num_samples}_{gpu}x{num_gpu}"
     name += f"_bs{batch_size}"
     return name
 
