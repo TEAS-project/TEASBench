e4c6d868eb3c0973ade29773222f964d7bea29b4
William Lucas <w.lucas@epcc.ed.ac.uk>
2026-06-23 13:19:05 +0100

    Vast.ai: Cleaning up and dealing with a few errors.

--- pipeline/vast/vllm/run_benchmarks.sh
+++ pipeline/vast/vllm/run_benchmarks.sh
@@ -12,34 +12,43 @@ echo "$CONTAINER_CSV"
 
 set -e
 
+start_timestamp=$( date +%Y%m%d-%H%M )
+BASE_DIR=/dev/shm/$start_timestamp
+mkdir -p "$BASE_DIR"
+cd "$BASE_DIR"
+
+exec &> "$BASE_DIR/stdout_stderr.log"
+
 # Function to run single benchmark with given options. Extra arguments might be
 # judge arguments for arena-hard or set the batch size, but for others this
 # argument might just be empty.
 run_benchmark() {
-    local dataset=$1
-    local num_samples=$2
-    local extra_args=$3
+    local run_dir=$1
+    local model_name=$2
+    local dataset=$3
+    local num_samples=$4
+    local extra_args=$5
 
     echo "Starting to run $dataset benchmark (sending http requests)..."
     client_start_time=$(date +%s)
     python3 -m moe_cap.runner.openai_api_profile \
-      --model_name unsloth/$MODEL_NAME \
+      --model_name unsloth/$model_name \
       --datasets $dataset \
       --num-samples $num_samples \
       --api-url http://localhost:30000/v1/completions \
       --use-chat-api \
-      --output_dir $RUN_DIR \
+      --output_dir $run_dir \
       --backend vllm \
       $extra_args \
-      &> $RUN_DIR/client_$dataset.log
+      &> "$run_dir/client_$dataset.log"
     CLIENT_SUCCESS=$?  # not 100% robust, could be failed redirection
 
-    if [ "$CLIENT_SUCCESS" == 0 ]; then
+    if [[ $CLIENT_SUCCESS -eq 0 ]]; then
         echo "$dataset benchmark run finished"
         client_end_time=$(date +%s)
         client_duration=$((client_end_time - client_start_time))
         echo "Client took $client_duration seconds"
-        jq -n --arg client_duration $client_duration '{client: $client_duration}'  >> $RUN_DIR/timings.json
+        jq -n --arg client_duration $client_duration '{client: $client_duration}'  >> "$run_dir/timings.json"
     else
         echo "Client crashed"
     fi
@@ -74,7 +83,7 @@ while IFS=',' read -r -a VALUES; do
     ((line_number++))
 
     # Verify that we have the same number of entries as headers.
-    if [ "${#VALUES[@]}" -ne "${#HEADERS[@]}" ]; then
+    if [[ "${#VALUES[@]}" -ne "${#HEADERS[@]}" ]]; then
         echo "ERROR on line $line_number: Column count mismatch" >&2
         exit 1
     fi
@@ -107,15 +116,16 @@ while IFS=',' read -r -a VALUES; do
     # The parameters for this benchmark look good, so go ahead and run it.
     echo "Running ${row[inference_engine]} ${row[model]} ${row[dataset]} ${row[num_samples]} ${row[gpu]} ${row[num_gpu]} ${row[batch_size]}"
 
-    job_start_time=$(date +%s)
+    run_start_time=$(date +%s)
+    SERVER_STATUS=""
 
-    timestamp=$( date +%Y%m%d-%H%M )
-    RUN_NAME="moe/vast/${row[inference_engine]}/${row[model_name]}/${row[dataset]}_${row[num_samples]}samples/${row[gpu]}x${row[num_gpu]}/batch-size-${row[batch_size]}/$timestamp"
-    RUN_DIR=/dev/shm/$timestamp
-    mkdir -p $RUN_DIR
-    cd $RUN_DIR
+    cd "$BASE_DIR"
 
-    exec &> $RUN_DIR/stdout_stderr.log
+    run_timestamp=$( date +%Y%m%d-%H%M )
+    RUN_DIR="${BASE_DIR}/moe/vast/${row[inference_engine]}/${row[model]}/${row[dataset]}_${row[num_samples]}samples/${row[gpu]}x${row[num_gpu]}/batch-size-${row[batch_size]}/$run_timestamp"
+    # RUN_DIR=/dev/shm/$timestamp
+    mkdir -p "$RUN_DIR"
+    cd "$RUN_DIR"
 
     # Extra arguments for the server.
     if [[ "${row[batch_size]}" == "1" ]]; then
@@ -127,13 +137,13 @@ while IFS=',' read -r -a VALUES; do
     echo "Starting server..."
     server_init_start_time=$(date +%s)
     python3 -m moe_cap.systems.vllm \
-      --model unsloth/$MODEL_NAME \
+      --model unsloth/${row[model]} \
       --port 30000 \
       --host 0.0.0.0 \
       --enable-expert-distribution-metrics \
       --tensor-parallel-size 1 \
-      $extra_args \
-      --reasoning-parser openai_gptoss &> $RUN_DIR/server.log &
+      $server_extra_args \
+      --reasoning-parser openai_gptoss &> "$RUN_DIR/server.log" &
     SERVER_PID=$!
 
     # Wait until the /health endpoint returns HTTP 200
@@ -152,18 +162,18 @@ while IFS=',' read -r -a VALUES; do
     done
 
     # Server has started, so run the benchmark.
-    if [ "$SERVER_STATUS" != "dead" ]; then
+    if [[ "$SERVER_STATUS" != "dead" ]]; then
         server_init_end_time=$(date +%s)
         server_init_duration=$((server_init_end_time - server_init_start_time))
         echo "Server is ready!"
         echo "Server initialisation took $server_init_duration seconds"
-        jq -n --arg server_init_duration $server_init_duration '{server_initialisation: $server_init_duration}'  >> $RUN_DIR/timings.json
+        jq -n --arg server_init_duration $server_init_duration '{server_initialisation: $server_init_duration}'  >> "$RUN_DIR/timings.json"
 
         # If we'll be benchmarking arena-hard, download the sample answers before we
         # get going.
         if [[ "${row[dataset]}" == "arena-hard" ]]; then
             echo "Downloading sample answers for arena-hard..."
-            curl -L --output-dir $RUN_DIR -O https://raw.githubusercontent.com/lmarena/arena-hard-auto/main/data/arena-hard-v0.1/model_answer/gpt-4-0613.jsonl
+            curl -L --output-dir "$BASE_DIR" -O https://raw.githubusercontent.com/lmarena/arena-hard-auto/main/data/arena-hard-v0.1/model_answer/gpt-4-0613.jsonl
         fi
 
         # Determine the extra arguments to the client.
@@ -176,14 +186,14 @@ while IFS=',' read -r -a VALUES; do
                 --judge-api-url https://openrouter.ai/api/v1/chat/completions \
                 --judge-model openai/gpt-4.1 \
                 --judge-api-key $OPENROUTER_API_KEY \
-                --baseline-answers-path $RUN_DIR/gpt-4-0613.jsonl"
+                --baseline-answers-path $BASE_DIR/gpt-4-0613.jsonl"
         fi
         echo "Benchmarking with extra arguments: $extra_args"
-        run_benchmark "${row[dataset]}" "${row[num_samples]}" "$extra_args"
+        run_benchmark "$RUN_DIR" "${row[model]}" "${row[dataset]}" "${row[num_samples]}" "$extra_args"
         RUN_SUCCESS=$?
 
         # If run is successful, stage results and push to repo. Otherwise, skip.
-        if [ $RUN_SUCCESS -eq 0 ]; then
+        if [[ $RUN_SUCCESS -eq 0 ]]; then
             # push_results $dataset
             echo "Benchmark succeeded; push results to repo here."
         else
@@ -201,4 +211,7 @@ while IFS=',' read -r -a VALUES; do
 
 done < <(echo "$CONTAINER_CSV" | base64 -d | tail -n +2)
 
-echo "Done!"
+benchmark_end_timestamp=$( date +%Y%m%d-%H%M )
+echo "-----------------------------------------"
+echo "Benchmarking completed at $benchmark_end_timestamp."
+echo "-----------------------------------------"
